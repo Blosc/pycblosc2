15e6c33441a478509be2bbb63b6e637cd90d59d5
Francesc Alted <faltet@gmail.com>
2018-02-08 12:00:25 +0100

    First public version

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # pycblosc2
-A simple Python/CFFI interface for the C-Blosc2 library 
+
+
+
+A simple Python/CFFI interface for the C-Blosc2 library.
+
+This tries to be a low level interface for C-Blosc2.  Maybe in the future more high-level function could be added too.
+
+This package is meant to be used in Python3.  Python is not tested at all, so use it at your own risk.
+
+## Simple usage
+
+```
+
+In [1]: import numpy as np
+
+In [2]: import pycblosc2 as cb2
+
+# Create an input buffer
+In [3]: a = np.arange(1000_000, dtype=np.int32)
+
+# Create a buffer for compression
+In [4]: b = np.empty(1000_000, dtype=np.int32)
+
+# Create a buffer for decompression
+In [5]: c = np.empty(1000_000, dtype=np.int32)
+
+# Compress!
+In [6]: cb2.blosc_compress(7, 1, a.dtype.itemsize, a.size * a.dtype.itemsize, a, b, b.size * b.dtype.itemsize)
+Out[6]: 36256
+
+# Decompress!
+In [7]: cb2.blosc_decompress(b, c, c.size * c.dtype.itemsize)
+Out[7]: 4000000
+
+# Check for equality
+In [8]: np.testing.assert_array_equal(a, c)
+
+```
+
+## Testing
+
+```
+$ python tests.py
+```
+
+
+## Authors
+
+* **Aleix Alcacer**
+
+* **Francesc Alted**
+
---
+++ tests.py
@@ -0,0 +1,77 @@
+import pycblosc2 as cb2
+import numpy as np
+import unittest
+
+
+class TestUM(unittest.TestCase):
+
+    def setUp(self):
+        self.arr_1 = np.random.randint(5, size=(1000, 1000), dtype=np.int32)
+        self.arr_2 = np.random.randint(5, size=(1000, 1000), dtype=np.int32)
+        self.arr_3 = np.random.randint(5, size=(1000, 1000), dtype=np.int32)
+        self.arr_aux = np.random.randint(5, size=(100, 100), dtype=np.int32)
+
+
+    def test_compress_decompress(self):
+        cb2.blosc_compress(5, 1, 4, 1000000 * 4, self.arr_1, self.arr_2, 1000000 * 4)
+        cb2.blosc_decompress(self.arr_2, self.arr_3, 1000000 * 4)
+        np.testing.assert_array_equal(self.arr_1, self.arr_3)
+
+
+    def test_compress_getitem(self):
+        cb2.blosc_compress(5, 0, 4, 1000000 * 4, self.arr_1, self.arr_2, 1000000 * 4)
+        cb2.blosc_getitem(self.arr_2, 1000, 10000, self.arr_aux)
+        arr_1 = self.arr_1[1:11, :].reshape(100, 100)
+        np.testing.assert_array_equal(arr_1, self.arr_aux)
+
+
+    def test_threads(self):
+        cb2.blosc_set_nthreads(2)
+        n = cb2.blosc_get_nthreads()
+        self.assertEqual(n, 2)
+
+
+    def test_compressor(self):
+        cb2.blosc_set_compressor("lz4hc")
+        n = cb2.blosc_get_compressor()
+        self.assertEqual(n, "lz4hc")
+
+
+    def test_ratio(self):
+        bcomp = cb2.blosc_compress(5, 1, 4, 1000000 * 4, self.arr_1, self.arr_2, 1000000 * 4)
+        ratio = (1000000 * 4) / float(bcomp)
+        self.assertGreater(ratio, 5)
+
+
+    def test_blosc_options(self):
+        cb2.blosc_compress(5, 0, 4, 1000000 * 4, self.arr_1, self.arr_2, 1000000 * 4)
+        _, flag = cb2.blosc_cbuffer_metainfo(self.arr_2)
+        self.assertEqual(flag, [0, 0, 0, 0])
+        cb2.blosc_set_delta(1)
+        cb2.blosc_compress(5, 1, 4, 1000000 * 4, self.arr_1, self.arr_2, 1000000 * 4)
+        _, flag = cb2.blosc_cbuffer_metainfo(self.arr_2)
+        self.assertEqual(flag, [1, 0, 0, 1])
+        cb2.blosc_set_delta(0)
+        cb2.blosc_compress(5, 2, 4, 1000000 * 4, self.arr_1, self.arr_2, 1000000 * 4)
+        _, flag = cb2.blosc_cbuffer_metainfo(self.arr_2)
+        self.assertEqual(flag, [0, 1, 0, 0])
+
+
+    def test_context(self):
+        cparams = cb2.blosc2_create_cparams(compcode = 1, clevel = 5, use_dict = 0, typesize = 4,
+                                            nthreads = 1, blocksize = 0, schunk = None,
+                                            filters = [0, 0, 0, 0, 1],
+                                            filters_meta = [0, 0, 0, 0, 0])
+        cctx = cb2.blosc2_create_cctx(cparams)
+        dparams = cb2.blosc2_create_dparams(nthreads=1, schunk=None)
+        dctx = cb2.blosc2_create_dctx(dparams)
+        cb2.blosc2_compress_ctx(cctx, 1000000 * 4, self.arr_1, self.arr_2, 1000000 * 4)
+        cb2.blosc2_decompress_ctx(dctx, self.arr_2, self.arr_3, 1000000 * 4)
+        np.testing.assert_array_equal(self.arr_1, self.arr_3)
+        cb2.blosc2_getitem_ctx(dctx, self.arr_2, 1000, 10000, self.arr_aux)
+        arr_1 = self.arr_1[1:11, :].reshape(100, 100)
+        np.testing.assert_array_equal(arr_1, self.arr_aux)
+
+
+if __name__ == '__main__':
+    unittest.main()
