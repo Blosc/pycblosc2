f93423084a00870e5ecef831d5305a3a99f82218
Francesc Alted <faltet@gmail.com>
2018-02-09 09:59:04 +0100

    Use the actual version in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
+import setuptools_scm
+from setuptools_scm import get_version
 from distutils.core import setup
 
-setup(name='pycblosc2',
-      use_scm_version=True,
-      setup_requires=['setuptools_scm'],
-      py_modules=['pycblosc2'],
-      )
+
+setup(
+    name='pycblosc2',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    version=get_version(),
+    py_modules=['pycblosc2'],
+    )
